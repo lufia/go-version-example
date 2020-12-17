846473ef52c8fac6a22c3b6de2c76e7297992242
lufia <lufia@lufia.org>
2020-12-17 13:48:34 +0900

    v0.0.1
    % go run main.go
    Main = (devel)

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Fatalln("can't get BuildInfo")
+	}
+	fmt.Printf("Main = %s\n", info.Main.Version)
+	for _, m := range info.Deps {
+		fmt.Printf("Deps[%s] = %+v\n", m.Path, m.Version)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+
+	_ "github.com/google/go-cmp/cmp"
+)
+
+func TestVersion(t *testing.T) {
+}
